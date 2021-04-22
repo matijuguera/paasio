767ecf2717e6bf4cbc8c64cfd48e083e267b34ee
Matias Juguera <mati.juguera@gmail.com>
2021-04-21 22:19:06 -0300

    pass TestWriteWriter

--- paasio.go
+++ paasio.go
@@ -1,1 +1,15 @@
 package paasio
+
+import "io"
+
+type MyWriteCounter struct {
+	io.Writer
+}
+
+func (w MyWriteCounter) WriteCount() (n int64, nops int) {
+	return 0, 0
+}
+
+func NewWriteCounter(w io.Writer) WriteCounter {
+	return MyWriteCounter{w}
+}
