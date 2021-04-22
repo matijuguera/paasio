feb7e83687e0b9e0e8341993d68ddfc32e1a72e0
Matias Juguera <mati.juguera@gmail.com>
2021-04-22 00:08:14 -0300

    refactor

--- paasio.go
+++ paasio.go
@@ -5,34 +5,20 @@ import (
 	"sync"
 )
 
-type MyWriteCounter struct {
-	io.Writer
-	counter
-}
-
-func (w *MyWriteCounter) WriteCount() (n int64, nops int) {
-	return w.count()
-}
-
-func (rc *MyWriteCounter) Write(p []byte) (int, error) {
-	m, err := rc.Writer.Write(p)
-	rc.addBytes(m)
-	return m, err
-}
-
 type MyReadCounter struct {
 	io.Reader
-	counter
+	Counter
 }
 
-func (r *MyReadCounter) ReadCount() (n int64, nops int) {
-	return r.count()
+type MyWriteCounter struct {
+	io.Writer
+	Counter
 }
 
-func (rc *MyReadCounter) Read(p []byte) (int, error) {
-	m, err := rc.Reader.Read(p)
-	rc.addBytes(m)
-	return m, err
+type Counter struct {
+	bytes int64
+	ops   int
+	sync.RWMutex
 }
 
 type MyReadWriteCounter struct {
@@ -40,35 +26,49 @@ type MyReadWriteCounter struct {
 	ReadCounter
 }
 
-type counter struct {
-	bytes int64
-	ops   int
-	sync.RWMutex
-}
-
-func (c *counter) count() (int64, int) {
-	c.RLock()
-	defer c.RUnlock()
-	return c.bytes, c.ops
-}
-
-func (c *counter) addBytes(n int) {
-	c.Lock()
-	defer c.Unlock()
-	c.bytes += int64(n)
-	c.ops++
+func NewReadCounter(r io.Reader) ReadCounter {
+	return &MyReadCounter{r, Counter{}}
 }
 
 func NewWriteCounter(w io.Writer) WriteCounter {
-	return &MyWriteCounter{w, counter{}}
-}
-
-func NewReadCounter(r io.Reader) ReadCounter {
-	return &MyReadCounter{r, counter{}}
+	return &MyWriteCounter{w, Counter{}}
 }
 
 func NewReadWriteCounter(rw io.ReadWriter) ReadWriteCounter {
 	return MyReadWriteCounter{
 		NewWriteCounter(rw), NewReadCounter(rw),
 	}
+}
+
+func (r *MyReadCounter) ReadCount() (n int64, nops int) {
+	return r.Count()
+}
+
+func (rc *MyReadCounter) Read(b []byte) (int, error) {
+	m, err := rc.Reader.Read(b)
+	rc.AddBytes(m)
+	return m, err
+}
+
+func (w *MyWriteCounter) WriteCount() (n int64, nops int) {
+	return w.Count()
+}
+
+func (rc *MyWriteCounter) Write(b []byte) (int, error) {
+	m, err := rc.Writer.Write(b)
+	rc.AddBytes(m)
+	return m, err
+}
+
+func (c *Counter) Count() (int64, int) {
+	c.RLock()
+	defer c.RUnlock()
+	return c.bytes, c.ops
+}
+
+func (c *Counter) AddBytes(n int) {
+	c.Lock()
+	defer c.Unlock()
+	c.bytes += int64(n)
+	c.ops++
 }
