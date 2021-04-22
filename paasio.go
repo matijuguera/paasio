a4669443c22b8f51fa3da156a389687c28dc4530
Matias Juguera <mati.juguera@gmail.com>
2021-04-21 23:18:04 -0300

    pass TestReadReader

--- paasio.go
+++ paasio.go
@@ -1,23 +1,68 @@
 package paasio
 
-import "io"
+import (
+	"io"
+	"sync"
+)
 
 type MyWriteCounter struct {
 	io.Writer
-}
-type MyReadWriter interface {
-	io.Reader
-	io.Writer
+	counter
 }
 
-func (w MyWriteCounter) WriteCount() (n int64, nops int) {
-	return 0, 0
+func (w *MyWriteCounter) WriteCount() (n int64, nops int) {
+	return w.count()
+}
+
+type MyReadCounter struct {
+	io.Reader
+	counter
+}
+
+func (r *MyReadCounter) ReadCount() (n int64, nops int) {
+	return r.count()
+}
+
+func (rc *MyReadCounter) Read(p []byte) (int, error) {
+	m, err := rc.Reader.Read(p)
+	rc.addBytes(m)
+	return m, err
+}
+
+type MyReadWriteCounter struct {
+	WriteCounter
+	ReadCounter
+}
+
+type counter struct {
+	bytes int64
+	ops   int
+	sync.RWMutex
+}
+
+func (c *counter) count() (int64, int) {
+	c.RLock()
+	defer c.RUnlock()
+	return c.bytes, c.ops
+}
+
+func (c *counter) addBytes(n int) {
+	c.Lock()
+	defer c.Unlock()
+	c.bytes += int64(n)
+	c.ops++
 }
 
 func NewWriteCounter(w io.Writer) WriteCounter {
-	return MyWriteCounter{w}
+	return MyWriteCounter{w, counter{}}
 }
 
-func NewReadWriteCounter(rw MyReadWriter) WriteCounter {
-	return MyWriteCounter{rw}
+func NewReadCounter(r io.Reader) ReadCounter {
+	return &MyReadCounter{r, counter{}}
+}
+
+func NewReadWriteCounter(rw io.ReadWriter) ReadWriteCounter {
+	return MyReadWriteCounter{
+		NewWriteCounter(rw), NewReadCounter(rw),
+	}
 }
